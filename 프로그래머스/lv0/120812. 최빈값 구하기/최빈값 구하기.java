560afdc32ada8e4b9512df692beb05f183b26ac2
hggew <40794341+hggew@users.noreply.github.com>
2023-07-25 22:11:13 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.29 ms, Memory: 70.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.java
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -5,6 +5,7 @@ class Solution {
         HashMap<Integer, Integer> hm = new HashMap<>();
         
         //빈도수 구하기
+        //key-숫자 value-빈도수
         for (int i : array) {
             hm.put(i, hm.getOrDefault(i, 0)+1);
         }
@@ -12,6 +13,7 @@ class Solution {
         //최빈값 찾기
         int max = Collections.max(hm.values());
         int dupe = 0; //최빈값을 가진 숫자의 개수
+        
         //최빈값 중복 확인
         for (Map.Entry<Integer, Integer> e : hm.entrySet()) {
             if(max == e.getValue()) {
@@ -19,10 +21,10 @@ class Solution {
                 answer = e.getKey();   //최빈값 1개일때 정수 answer에 저장
             }            
         }
-        if(dupe>1) { //최빈값 여러개
+        if(dupe>1) { //최빈값 여러 개 일 때, -1 저장
         	answer = -1;           
         }
-        
+         
         return answer;
     }
 }
