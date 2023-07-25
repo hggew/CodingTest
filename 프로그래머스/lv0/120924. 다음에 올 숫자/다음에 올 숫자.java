bf0e08bb1b850fadde4fbdc4434048371934f447
hggew <40794341+hggew@users.noreply.github.com>
2023-07-25 22:38:52 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.02 ms, Memory: 66 MB -BaekjoonHub

--- 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -1,27 +1,34 @@
 class Solution {
     public int solution(int[] common) {
         int answer = 0;
-        int mode = 0; //mode 0 : 등차, mode 1 : 등비
-        int dif = common[1]- common[0];
+		int mode = 0; // mode 0 : 등차, mode 1 : 등비
 
-        int index = 2;
-        
-        while(index < common.length) {
-        	 if(dif == common[index]-common[index-1]) { //등차수열이라면
-        		 mode = 0;
-        		 index++;
-        	 }else {	//등비수열이라면
-        		 dif = common[index]/common[index-1];
-        		 mode = 1;
-        		 index++;
-        	 }
-        }
-        if(mode == 0) {
-        	answer = common[common.length-1] + dif;
-        }else {
-        	answer = common[common.length-1]*dif;
-        }
-         
+		// 1번원소와 0번 요소의 차이
+		int dif = common[1] - common[0];
+
+		// 배열에 사용할 인덱스
+		int index = 2;
+
+		// 배열의 길이만큼 반복
+		while (index < common.length) {
+			// 등차수열이라면 모드=0
+			if (dif == common[index] - common[index - 1]) {
+				mode = 0;
+			}
+			// 등비수열이라면 모드=1
+			else {
+				dif = common[index] / common[index - 1];
+				mode = 1;
+			}
+			index++;
+		}
+
+		// 등차수열, 등비수열일 때 answer 계산
+		if (mode == 0) {
+			answer = common[common.length - 1] + dif;
+		} else {
+			answer = common[common.length - 1] * dif;
+		}
         
         return answer;
     }
