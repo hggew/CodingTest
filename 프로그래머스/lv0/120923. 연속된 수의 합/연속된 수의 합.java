eee93447d891f53376bbd06b749c75b0df81d5ae
hggew <40794341+hggew@users.noreply.github.com>
2023-07-25 22:34:26 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.04 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.java
+++ 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.java
@@ -1,20 +1,23 @@
 class Solution {
     public int[] solution(int num, int total) {
+        //answer를 num의 길이만큼 할당
         int[] answer = new int[num];
-		int sum = 0;
+		
+        // total = firstNum*num + remainSum(0~num-1)
+        // remainSum= total - 첫번째 n*num(개수)의 값 
+        int remainSum = 0;
 		for (int i = 0; i < num; i++) {
-			sum += i;
+			remainSum += i;
 		}
 		
 		// answer에 들어갈 숫자의 첫번째 값
-		// total = firstNum*num + sum(0~num-1)
-		int firstNum = (total - sum) / num;
+		int firstNum = (total - remainSum) / num;
 
 		// answer에 첫번째 숫자부터 1씩 더해가며 num번째 숫자까지 구하기
 		for (int i = 0; i < num; i++) {
 			answer[i] = i+firstNum;
 		}	
-		
-		return answer;
+        
+        return answer;
     }
 }
