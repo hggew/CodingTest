ccb804216b9912d04372f5db9aa16a4efa9bdf2a
hggew <40794341+hggew@users.noreply.github.com>
2023-08-04 16:10:02 +0900

    [D4] Title: [S/W 문제해결 기본] 4일차 - 괄호 짝짓기, Time: 103 ms, Memory: 18,664 KB -BaekjoonHub

--- SWEA/D4/1218. ［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기/［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기.java
+++ SWEA/D4/1218. ［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기/［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기.java
@@ -1,56 +1,50 @@
 import java.io.*;
 import java.util.*;
 
-public class Solution {
+public class Solution  {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
+		
+		//key-괄호종류 value-종류에 따른 숫자 
+		//value가 짝수면 여는 괄호, 홀수면 닫는 괄호
 		HashMap<Character, Integer> pair = new HashMap<>();
 		pair.put('{', 0);
-		pair.put('}', 0);
-		pair.put('(', 1);
-		pair.put(')', 1);
-		pair.put('[', 2);
-		pair.put(']', 2);
-		pair.put('<', 3);
-		pair.put('>', 3);
+		pair.put('}', 1);
+		pair.put('(', 2);
+		pair.put(')', 3);
+		pair.put('[', 4);
+		pair.put(']', 5);
+		pair.put('<', 6);
+		pair.put('>', 7);
 
-		Set<Character> left = new HashSet<>();
-		left.add('<');
-		left.add('(');
-		left.add('[');
-		left.add('{');
-		Set<Character> right= new HashSet<>();
-		right.add('>');
-		right.add(')');
-		right.add(']');
-		right.add('}');
-		 
-		
-		
 		for (int T = 1; T <= 10; T++) {
 			int n = Integer.parseInt(br.readLine());
 			Deque<Character> bracket = new ArrayDeque<>();
-			int result  =1;
-			
+			int result = 1;
+
 			sb = new StringBuilder();
 			sb.append(br.readLine());
 			for (int i = 0; i < n; i++) {
-				//큐에 넣기
-				if(left.contains(sb.charAt(i))) {
-					bracket.add(sb.charAt(i));					
+				// 큐에 넣기 : 해시맵에서 value가 짝수라면 여는 괄호
+				if (pair.get(sb.charAt(i)) % 2 == 0) {
+					bracket.add(sb.charAt(i));
 				}
-				//큐에서 빼기
-				else if (right.contains(sb.charAt(i))) {
-					if(pair.get(bracket.peekLast()) == pair.get(sb.charAt(i)) ){
-						bracket.pollLast();						
-					}else {
+				// 큐에서 빼기 : 해시맵에서 value가 짝수라면 닫는 괄호
+				else  {
+					//현재닫는괄호와 큐의 last가 같은 종류라면 last poll
+					if (pair.get(bracket.peekLast())/2 == pair.get(sb.charAt(i))/2 ) {
+						bracket.pollLast();
+					} 
+					//아니라면 결과값 0으로 저장 후 출력 -> 다음 테스트케이스 확인
+					else {
 						result = 0;
 						break;
 					}
 				}
-			} 
+			}
+
 			sb = new StringBuilder();
 			sb.append("#").append(T).append(" ");
 			sb.append(result);
