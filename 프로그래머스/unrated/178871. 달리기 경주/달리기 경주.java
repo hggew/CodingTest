0ea9124037ab7151c2576411b4e6d4e4007a0f15
hggew <40794341+hggew@users.noreply.github.com>
2023-08-08 11:28:20 +0900

    [unrated] Title: 달리기 경주, Time: 0.51 ms, Memory: 67.5 MB -BaekjoonHub

--- 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.java
@@ -2,41 +2,37 @@ import java.util.*;
 
 class Solution {
 	public  String[] solution(String[]  players, String[] callings) {
-        String[] answer = new String[players.length]; 
-		
+        String[] answer = new String[players.length];
+
+		// key-선수이름, value-순위 : 추월 전 순위를 가져올 때 사용
 		HashMap<String, Integer> nameKey = new HashMap<>();
-		HashMap<Integer, String> rankKey = new HashMap<>();
-				 
+		// key-순위, value-선수이름 : 추월 전 내 앞 선수 이름을 가져올 때 사용
+        // 랭크를 오름차순으로 정렬하지 않기 위해 TreeMap 사용
+		Map<Integer, String> rankKey = new TreeMap<>();
+
 		for (int i = 0; i < players.length; i++) {
 			nameKey.put(players[i], i);
 			rankKey.put(i, players[i]);
 		}
-				 
-		for (int i = 0; i < callings.length; i++) { 
-			int beforeRank = nameKey.get(callings[i]);
-			String beforePlayer = rankKey.get(beforeRank-1);
-			
-			nameKey.put(callings[i], beforeRank-1);
-			nameKey.put(beforePlayer, beforeRank);
-			
-			rankKey.put(beforeRank, beforePlayer);
-			rankKey.put(beforeRank-1, callings[i]);
-//			System.out.println(callings[i] + beforeRank + beforePlayer);
-		}
-		
-//		System.out.println(nameKey);
-//		System.out.println(rankKey);
-		
-		List<Integer> rank = new ArrayList<>(rankKey.keySet());
-		Collections.sort(rank);
 
-		for (int i = 0; i < rank.size(); i++) {
-			answer[i] = rankKey.get(rank.get(i));
+		// callings의 원소를 하나씩 보면서 순위 갱신
+		for (int i = 0; i < callings.length; i++) {
+			// 추월전 순위, 추월 전 앞선수이름을 가져옴
+			int beforeRank = nameKey.get(callings[i]);
+			String beforePlayer = rankKey.get(beforeRank - 1);
+
+			// 순위 갱신
+			nameKey.put(callings[i], beforeRank - 1);
+			nameKey.put(beforePlayer, beforeRank);
+
+			rankKey.put(beforeRank, beforePlayer);
+			rankKey.put(beforeRank - 1, callings[i]);
 		}
-//		for (String st : answer) {
-//			System.out.println(st);
-//		}
-		
+
+		//rankKey 해시맵의 value를 answer로 저장
+		answer = rankKey.values().toArray(new String [players.length]);
+		 
+
 		return answer;
     }
 }
