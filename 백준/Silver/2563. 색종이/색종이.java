681bb3c7a09fc628e2592e700a89940eed66d8f1
hggew <40794341+hggew@users.noreply.github.com>
2023-08-09 22:43:25 +0900

    [Silver V] Title: 색종이, Time: 80 ms, Memory: 11608 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken()); //색종이 수
+		int[][] paper = new int[100][100];
+		for (int i = 0; i < paper.length; i++) {
+			for (int j = 0; j < paper.length; j++) {
+				paper[i][j] =0;
+			}
+		}
+		
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			for (int j = x; j < x+10; j++) {
+				for (int k = y; k < y+10; k++) {
+					paper[j][k] = 1;
+				}
+			}
+		}
+		int count =0;
+		for (int i = 0; i < paper.length; i++) {
+			for (int j = 0; j < paper.length; j++) {
+				if(paper[i][j] == 1)
+					count++;
+			}
+		}
+		System.out.println(count);
+		
+		
+	}
+
+}
