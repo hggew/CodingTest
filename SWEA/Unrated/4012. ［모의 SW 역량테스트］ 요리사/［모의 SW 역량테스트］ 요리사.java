10311a14447165b3e0c910c751b789ca83d57582
hggew <40794341+hggew@users.noreply.github.com>
2023-08-10 16:34:42 +0900

    [Unrated] Title: [모의 SW 역량테스트] 요리사, Time: 620 ms, Memory: 110,792 KB -BaekjoonHub

--- SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
+++ SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
@@ -2,17 +2,18 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-	static int[] foodA;	//조합 별 음식A의 재료 저장
-	static int[] foodB;	//조합 별 음식A의 재료 저장
-	static int sA,sB = 0; //각 음식의 시너지 
-	static List<Integer> difs; //음식의 시너지 차이를 저장
-	static int[] foodComps; //시너지를 구할 때 재료 i,j를 저장하는 배열
-	static int sum;
+	static int[] foodA;			//조합 별 음식A의 재료 저장
+	static int[] foodB;			//조합 별 음식A의 재료 저장
+	static int sA,sB = 0; 		//각 음식의 시너지 
+	static List<Integer> difs;	//음식의 시너지 차이를 저장
+	static int[] foodComps; 	//시너지를 구할 때 재료 i,j를 저장하는 배열
+	static int sum;				//음식의 시너지 총 합을 저장
 	
 	static int n; //식재료 수
 	static int[][] s;		//음식 간 시너지 정보
 	static int[] components;	//식재료 총모음
 	
+	//음식 재료 간 시너지를 구하는 함수
 	public static int getSynergy(char type, int cnt, int start) {
 		int[] food = new int[n/2];
 		// 음식 A, 음식B 중 어느 음식의 시너지 합을 구하는지 판단
@@ -24,12 +25,11 @@ public class Solution {
 				food = foodB;
 				break;
 		}
-
 		
+		//재료 중 2개를 뽑으면 시너지를 구해서 sum에 저장
 		if(cnt == 2) {
 			sum += s[foodComps[0]][foodComps[1]];
 			sum += s[foodComps[1]][foodComps[0]];
-//			System.out.println(foodComps[0] +"/"+foodComps[1] + " / " +sum);
 			return sum;
 		}
 		for (int i = start; i < n/2; i++) {
@@ -43,34 +43,27 @@ public class Solution {
 	public static void findComponents(int count, int start) {
 		//식재료 조합을 모두 구했으면 식재료간의 시너지 합 구하기
 		if(count == n/2) {
-			//음식B의 식재료 찾기
+			//음식B의 식재료 찾기 : 전체 음식 재료 components에서 음식A가 사용하지 않은 재료 선택
 			for (int i = 0, j = 0; i < n; i++) {
 				if(components[i] == 0)
 					foodB[j++]= i;
-			}   
-//			System.out.println("foodA / foodB");
-//			for (int i = 0; i < n/2; i++) {
-//				System.out.println(foodA[i] +"/"+foodB[i]);
-//			}
+			}    
 			
-			
-			sum = 0;
+			//음식A와 음식B의 시너지를 구하고 시너지 차이의 절댓값을 difs 리스트에 추가
+			sum = 0;	//이전 음식이 구한 시너지의 합 초기화
 			sA = getSynergy('A',0,0);
-			sum = 0;
+			sum = 0;	//이전 음식이 구한 시너지의 합 초기화
 			sB = getSynergy('B',0,0);
-//			System.out.println(sA +  " / "+ sB);
 			difs.add(Math.abs(sA-sB));
 			return;
 		}
 		
 		for (int i = start; i < n; i++) {
-			foodA[count] = i;
-			components[i] = 1; //방문처리
+			foodA[count] = i;	//음식 A의 재료 추가
+			components[i] = 1;	//방문처리
 			findComponents(count+1, i+1);	//다음 식재료 찾기
 			components[i] = 0; //방문처리 되돌리기
 			}
-			 
-		
 	}
 	
 	
@@ -93,15 +86,14 @@ public class Solution {
 			}
 			
 			components = new int[n];	//식재료 n개 할당
-			foodA = new int[n/2];
-			foodB = new int[n/2];
-			foodComps = new int [2];
+			foodA = new int[n/2];		//음식A의 식재료 n/2개 할당
+			foodB = new int[n/2];		//음식B의 식재료 n/2개 할당
+			foodComps = new int [2];	//음식의 시너지를 구할 때 사용하는 배열 : 2개를 뽑아서 시너지 구함
 			difs = new ArrayList<>();
 			findComponents(0, 0);
 			
 			//시너지 차이의 리스트 오름차순 정렬
 			Collections.sort(difs);
-//			System.out.println(difs);
 			
 			//출력
 			sb.append("#").append(t).append(" ").append(difs.get(0)).append("\n");
