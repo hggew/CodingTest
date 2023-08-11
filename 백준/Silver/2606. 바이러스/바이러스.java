2e9de6ab39f25fc21098271302b53ecdf779917c
hggew <40794341+hggew@users.noreply.github.com>
2023-08-11 09:43:06 +0900

    [Silver III] Title: 바이러스, Time: 76 ms, Memory: 11712 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -2,25 +2,28 @@ import java.io.*;
 import java.util.*;
 
 public class Main { 
-	static List<List<Integer>> tree;
-	static boolean[] isVisited;
+	static List<List<Integer>> tree;	//컴퓨터 연결을 저장하는 트리
+	static boolean[] isVisited;			//bfs에서 사용. 방문처리 담당
 	
 	static int bfs() {
 		Deque<Integer> q = new ArrayDeque<>();
-		int count = 0;
+		int count = 0;	//1번컴퓨터와 연결된 컴퓨터의 수
 		q.add(1);
 		isVisited[1] = true;
-		while(!q.isEmpty()) {
+		while(!q.isEmpty()) {	//더이상 연결된 컴퓨터가 없을 때까지 반복
 			int now = q.poll();
-			for (int i = 0; i < tree.get(now).size(); i++) {
+			//현재 컴퓨터와 연결되어있고 방문하지 않은 모든 컴퓨터 큐에 저장
+			for (int i = 0; i < tree.get(now).size(); i++) {	
+				//이미 방문한 컴퓨터라면 다음 컴퓨터 확인
 				if ( isVisited[tree.get(now).get(i)])
 					continue;
+				//방문하지 않은 컴퓨터라면 큐에 추가. 방문처리. 1번컴퓨터와연결되어있는 숫자 증가
 				else {
 					q.add(tree.get(now).get(i));
 					isVisited[tree.get(now).get(i)] = true;
 					count+=1; 
 				}
-			} 
+			}
 		}		
 		return count;
 	}
