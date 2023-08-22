72ce600e740bc42d13a945874f2742a3b52f138c
hggew <40794341+hggew@users.noreply.github.com>
2023-08-22 17:39:15 +0900

    [Gold V] Title: ABCDE, Time: 228 ms, Memory: 16272 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.java
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -1,20 +1,18 @@
 import java.io.*;
-import java.util.*;
- 
-
-public class Main {
+import java.util.*; 
+public class Main  {
 
 	static boolean[] isVisited;
 	static Node[] list;
-	
 	static boolean flag;
 
 	static void dfs(int cnt, int start, int n) {
+		//이어진 간선의 개수가 5가 되면 종료
 		if (cnt == 5) {
 			flag = true;
 			return;
 		}
-		
+		// 현재 사람의 친구관계 탐색. 이미 방문한 친구라면 다음 관계의 친구 탐색
 		for(Node temp = list[start]; temp!= null; temp= temp.next) {
 			if(!isVisited[temp.vertex]) {
 				isVisited[temp.vertex] = true;
@@ -24,6 +22,7 @@ public class Main {
 		}
 	}
 
+	//인접리스트의 노드
 	public static class Node{
 		int vertex;
 		Node next;
@@ -31,14 +30,15 @@ public class Main {
 			super();
 			this.vertex = vertex;
 			this.next = next;
-		} 
+		}
 	}
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(st.nextToken());	//사람 수
+		int m = Integer.parseInt(st.nextToken());	//친구 관계 수
 
 		// 인접 리스트 입력
 		list = new Node[n];
@@ -46,20 +46,24 @@ public class Main {
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
+			//양방향 연결
 			list[a] = new Node(b, list[a]);
 			list[b] = new Node(a, list[b]);
 		} 
 		
+		//모든 사람의 친구관계를 확인
 		for (int i = 0; i < n; i++) {
 			isVisited = new boolean[n];
 			isVisited[i] = true;
 			flag = false;
 			dfs(1, i, n);
+			//주어진 친구관계를 가진 사람이 있다면 종료
 			if (flag) {
 				System.out.println(1);
 				return;
 			}
 		}
+		//모든 사람을 확인한 후에도 주어진 친구관계를 가진 사람이 없다면 0 출력
 		System.out.println(0);
 
 	}
