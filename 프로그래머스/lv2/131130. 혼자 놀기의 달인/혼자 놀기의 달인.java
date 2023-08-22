492ee374250fb5d184018ca0d4c88efb3fdd3cd4
hggew <40794341+hggew@users.noreply.github.com>
2023-08-22 10:31:32 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 1.49 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/lv2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
+++ 프로그래머스/lv2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
@@ -5,7 +5,6 @@ public class Solution {
 	static int n, maxScore;
 	static boolean[] isVisited;
 	
-	
 	static void openBox(int start, int cnt, int[] card) {
 		//처음 열어볼 상자 2개를 선택했다면 게임 시작
 		if (cnt == 2) {
@@ -14,27 +13,27 @@ public class Solution {
 			score[1] = 0;
 			isVisited= new boolean[n];	// 각 라운드 박스 방문 여부 초기화
 			for (int i=0; i<2; i++) {
-				 
-				int first = boxNum[i];
-				isVisited[first] = true;
-				int next = card[boxNum[i]]-1;
-				score[i]++;
+				int first = boxNum[i];          // 첫번째 박스 열기
+				isVisited[first] = true;        // 첫번째 박스 방문 처리
+				int next = card[boxNum[i]]-1;   // 다음으로 열어볼 박스 
+				score[i]++;                     // 박스 열어본 횟수 증가
+                //다음에 열어볼 박스가 첫번째 박스가 될 때까지 반복
 				while(first!= next) {
-					if(isVisited[next])
+					if(isVisited[next]) //다음에 열어볼 박스가 이미 방문한 박스라면 종료
 						break;
-					isVisited[next]= true;
-					score[i]++;
-					if(score[i] == n)
+					isVisited[next]= true;  //방문처리
+					score[i]++;             
+					if(score[i] == n)       //박스를 모두 열어봤다면 게임 종료. 점수 = 0
 						return;
-					next = card[next]-1;
+					next = card[next]-1;    //다음에 열어볼 박스 구하기
 				}
 			}
 			//최고값 갱신
 			maxScore = maxScore < score[0]*score[1] ? score[0]*score[1]: maxScore;
-			 
 			return;
 		}
 
+        //상자를 열어볼 순서 조합 구하기
 		for (int i = start; i < n; i++) {
 			boxNum[cnt] = i;
 			openBox(i+1, cnt+1, card);
@@ -44,11 +43,11 @@ public class Solution {
 
 	public static int solution(int[] cards) {
 
-		boxNum = new int[2];
-		score = new int[2];
-		maxScore = 0;
-		n = cards.length;
-		isVisited= new boolean[n];
+		boxNum = new int[2];    //박스를 열어볼 순서 배열
+		score = new int[2];     //박스를 열어본 횟수를 저장할 배열
+		maxScore = 0;           //게임 최고 점수
+		n = cards.length;       //카드의 개수
+		isVisited= new boolean[n];  // 각 라운드 박스 방문 여부 
 
 		openBox(0,0, cards); 
 		return maxScore;
