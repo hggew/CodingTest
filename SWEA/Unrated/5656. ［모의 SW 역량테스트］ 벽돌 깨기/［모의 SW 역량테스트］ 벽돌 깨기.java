b07dd927da7404a0475eda5b300f24db583d9884
hggew <40794341+hggew@users.noreply.github.com>
2023-10-05 16:41:10 +0900

    [Unrated] Title: [모의 SW 역량테스트] 벽돌 깨기, Time: 219 ms, Memory: 47,156 KB -BaekjoonHub

--- SWEA/Unrated/5656. ［모의 SW 역량테스트］ 벽돌 깨기/［모의 SW 역량테스트］ 벽돌 깨기.java
+++ SWEA/Unrated/5656. ［모의 SW 역량테스트］ 벽돌 깨기/［모의 SW 역량테스트］ 벽돌 깨기.java
@@ -13,10 +13,11 @@ public class Solution {
 		int T = Integer.parseInt(st.nextToken()); // 테스트케이스
 		for (int t = 1; t <= T; t++) {
 			st = new StringTokenizer(br.readLine());
-			int n = Integer.parseInt(st.nextToken());
-			w = Integer.parseInt(st.nextToken());
-			h = Integer.parseInt(st.nextToken());
+			int n = Integer.parseInt(st.nextToken());	//구슬개수
+			w = Integer.parseInt(st.nextToken());		//게임판 가로 길이
+			h = Integer.parseInt(st.nextToken());		//게임판 세로 길이
 
+			//게임판 입력
 			arr = new int[h][w];
 			for (int i = 0; i < h; i++) {
 				st = new StringTokenizer(br.readLine());
@@ -25,13 +26,13 @@ public class Solution {
 				}
 			}
 
-			minBlock = Integer.MAX_VALUE;
+			minBlock = Integer.MAX_VALUE;	//구슬을 다 쏘고 남은 벽돌의 개수 중 최소값
 			shoot(n, arr);
 
 			sb.append("#").append(t).append(" ");
 			sb.append(minBlock).append("\n");
 		}
-//		System.out.println("----------------");
+		
 		System.out.println(sb.toString());
 	}
 
@@ -54,14 +55,18 @@ public class Solution {
 			// 지도 복사
 			int[][] temp = copyMap(map);
 			
+			//현재 가로줄에서 블록이 있는 위치 찾기
 			int j = 0;
 			while (true) { 
+				//현재 위치가 0이라면 아래 세로줄로 내려가서 블록 찾기
 				if (temp[j][i] == 0)
 					j++;
 				else {
 					break;
 				}
+				//현재 세로 줄에 남은 블록이 없다면 다음 가로줄로 넘어가기
 				if(j==h) {
+					//모든 게임판을 다 순회한 경우 = 게임판에 남은 블록이 없음 => 구슬 개수 0으로 만들기
 					if(i==w-1) {
 						shoot(0, map);
 						n= 0;
@@ -72,32 +77,19 @@ public class Solution {
 				}
 			}
 
-			removeBlock(j, i, temp[j][i] - 1, temp); // 블럭없애기
-//			for (int ii = 0; ii < h; ii++) {
-//				for (int jj = 0; jj < w; jj++) {
-//					System.out.print(temp[ii][jj] + " ");
-//				}
-//				System.out.println();
-//			}
-//			System.out.println("------");
-			temp = rearrange(temp); // 게임판 재정렬
-//			for (int ii = 0; ii < h; ii++) {
-//				for (int jj = 0; jj < w; jj++) {
-//					System.out.print(temp[ii][jj] + " ");
-//				}
-//				System.out.println();
-//			}
-//			System.out.println("==========");
+			removeBlock(j, i, temp[j][i] - 1, temp); // 블럭없애기 
+			temp = rearrange(temp); // 게임판 재정렬 
 			shoot(n - 1, temp); // 다음 구슬 쏘기
 
 		}
-
 	}
 
+	//블럭을 제거하는 함수
 	static void removeBlock(int r, int c, int num, int[][] temp) {
 
 		// 가로줄 없애기
 		for (int i = c - num; i <= c + num; i++) {
+			//게임판 범위를 벗어나거나 현재 위치가 0이라면 다음칸 확인
 			if (!checkIndex(r, i) || temp[r][i] == 0)
 				continue;
 			if (temp[r][i] != 1 && i != c)
@@ -109,6 +101,7 @@ public class Solution {
 
 		// 세로줄 없애기
 		for (int i = r - num; i <= r + num; i++) {
+			//게임판 범위를 벗어나거나 현재 위치가 0이라면 다음칸 확인
 			if (!checkIndex(i, c) || temp[i][c] == 0)
 				continue;
 			if (temp[i][c] != 1 && i != r)
@@ -140,16 +133,17 @@ public class Solution {
 				}
 			}
 		}
-
-		return temp;
+		return temp;	//재정렬된 배열 반환
 	}
 
+	// 현재 좌표가 게임판 범위 내에 있는지 확인하는 함수
 	static boolean checkIndex(int x, int y) {
 		if (0 <= x && x < h && 0 <= y && y < w)
 			return true;
 		return false;
 	}
 
+	// 게임판 배열 복사하는 함수
 	static int[][] copyMap(int map[][]) {
 		int[][] temp = new int[h][w];
 		for (int a = 0; a < h; a++) {
